86cb182fc98cb60ecf4858c238d31aa8226b8cd7
xiaotiandada <istianlei@qq.com>
2021-09-27 01:04:56 +0800

    feat: top

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,6 +1,8 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { StyleSheet, Button as ButtonNative, Image as ImageNative, Modal, Alert, Pressable, TouchableOpacity, RefreshControl } from 'react-native';
-import { ScrollView, Box, Button, Image, Flex, FlatList } from 'native-base';
+import React, { useCallback, useEffect, useState, useRef } from 'react';
+import { StyleSheet, Button as ButtonNative, Image as ImageNative, Modal, Alert, Pressable, TouchableOpacity, RefreshControl, ActivityIndicator } from 'react-native';
+import { ScrollView, Box, Button, Image, Flex, FlatList, Fab } from 'native-base';
+import styled from 'styled-components';
+import { FontAwesome } from "@expo/vector-icons";
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -14,13 +16,14 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'Home'>)
   const [modalImageIndex, setModalImageIndex] = useState(0);
   const [imageData, setImageData] = useState<ImageDataState[]>([] as ImageDataState[]);
   const [refreshing, setRefreshing] = useState(false);
+  const ref = useRef<any>(null)
 
   const onRefresh = useCallback(
     async () => {
-    setRefreshing(true);
-    await fetchCosplay()
-    setRefreshing(false)
-  }, []);
+      setRefreshing(true);
+      await fetchCosplay()
+      setRefreshing(false)
+    }, []);
 
   /**
    * fetch cosplay
@@ -32,7 +35,16 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'Home'>)
         const list = data.map(i => ({ url: i }))
         setImageData(list)
       }
-  }, [imageData])
+    }, [imageData])
+
+  const fetchCosplayMore = useCallback(
+    async () => {
+      const data = await fetchCosplayAPI()
+      if (data) {
+        const list = data.map(i => ({ url: i }))
+        setImageData(imageData.concat(list))
+      }
+    }, [imageData])
 
   useEffect(() => {
     fetchCosplay()
@@ -47,41 +59,55 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'Home'>)
   const handleImageShow = useCallback((idx: number) => {
     setModalImageIndex(idx)
     setModalVisible(true)
-  }, [ modalVisible, modalImageIndex])
+  }, [modalVisible, modalImageIndex])
+
+  const renderLoadMoreView = () => {
+    return <View style={styles.loadMore}>
+      <ActivityIndicator
+        style={styles.indicator}
+        size={"small"}
+        animating={true}
+      />
+      <Text>Loading...</Text>
+    </View>
+  }
+
+  const goToTop = useCallback(() => {
+    ref.current.scrollToOffset({ offset: 0 });
+  }, [ref])
 
   return (
     <View style={styles.container}>
-      <ScrollView
-        _contentContainerStyle={{
-        }}
+      <FlatList
+        ref={ref}
+        numColumns={2}
+        data={imageData}
         refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} /> }
-      >
-        <Flex
-          direction="row"
-          flexWrap="wrap"
-          justifyContent="space-between"
-        >
-          {
-            imageData.map((i, idx) => (
-              <Pressable
-                key={`key${idx}-${i.url}`}
-                style={styles.item}
-                onPress={() => handleImageShow(idx)}
-              >
-                <Image
-                  source={{
-                    uri: i.url,
-                  }}
-                  alt="Alternate Text"
-                  width="100%"
-                  height="300px"
-                />
-              </Pressable>
-            ))
-          }
-        </Flex>
-      </ScrollView>
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        ListFooterComponent={() => renderLoadMoreView()}
+        onEndReached={() => fetchCosplayMore()}
+        renderItem={({ item, index }) => (
+          <StyledPressable
+            key={`key${index}-${item.url}`}
+            style={{
+              marginRight: index % 2 === 0 ? 5 : 0,
+              marginLeft: index % 2 !== 0 ? 5 : 0,
+              marginBottom: 10
+            }}
+            onPress={() => handleImageShow(index)}
+          >
+            <Image
+              source={{
+                uri: item.url,
+              }}
+              alt="Alternate Text"
+              width="100%"
+              height="300px"
+            />
+          </StyledPressable>
+        )} />
+
       <Modal visible={modalVisible} transparent={true}
         onRequestClose={() => {
           Alert.alert("Modal has been closed.");
@@ -89,23 +115,26 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'Home'>)
         }}>
         <ImageViewer imageUrls={imageData} onClick={() => setModalVisible(false)} index={modalImageIndex} />
       </Modal>
+
+      <Fab
+        position="absolute"
+        size="sm"
+        right={'20px'}
+        bottom={'100px'}
+        onPress={goToTop}
+        colorScheme="indigo"
+        icon={
+          <FontAwesome size={24} name="arrow-up" color={'#fff'} />
+        }
+      />
     </View>
   );
 }
 
-{/* <Text style={styles.title}>Hello 123 312 111</Text>
-        <Box>Hello world</Box>
-        <Button onPress={() => console.log("hello world")}>Primary</Button>
-        <ButtonNative onPress={() => console.log("hello world")} title="Primary ButtonNative"></ButtonNative>
-        <Text style={styles.title}>Tab One</Text>
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-        <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%'
   },
   title: {
     fontSize: 20,
@@ -116,10 +145,19 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-  item: {
-    width: '49%',
-    height: 300,
-    marginBottom: 8,
-    backgroundColor: "#DDDDDD"
+  loadMore: {
+    alignItems: "center",
+    paddingTop: 10,
+    paddingBottom: 20,
+  },
+  indicator: {
+    color: "#f1f1f1",
+    margin: 10
   }
 });
+
+const StyledPressable = styled(Pressable)`
+  width: 50%;
+  height: 300;
+  background-color: #f1f1f1;
+`;
